f42a2ba3d92425f779e50cb597c45449da44b5c3
s265780 <simone7monaco@gmail.com>
2021-11-30 13:59:50 +0000

    analysis on visirun

--- merger.py
+++ merger.py
@@ -19,18 +19,23 @@ def merge_json(path):
                         desc=f"{path.parent.stem}/{path.stem}"
                  ):
         try:
-            df = df.append(pd.read_json(f)) #TODO: da fare meglio, questo permette di aggregare dati come VISIRUN
+            tmp = pd.read_json(f)
+            tmp["filename"] = f.stem.rsplit('-', 1)[0]
+            df = df.append(tmp) #TODO: da fare meglio, questo permette di aggregare dati come VISIRUN
         except:
-            df = df.append(pd.read_json(f, typ='series'), ignore_index=True)
+            tmp = pd.read_json(f, typ='series')
+            tmp["filename"] = f.stem.rsplit('-', 1)[0]
+            df = df.append(tmp, ignore_index=True)
         
     return df
 
 for fornitore in Path("SEA Data Lake").iterdir():
+    if fornitore.name != "MOVIMATICA": continue
     if fornitore.name in ignore_list: continue
 
     for tab in fornitore.iterdir():
         if not tab.is_dir(): continue
         output = dataset / f"{fornitore.stem}_{tab.stem}.csv"
-        if output.exists(): continue
+#         if output.exists(): continue
             
         merge_json(tab).to_csv(output)
